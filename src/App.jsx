57674b1bae361d7bbff5624ecb6e212a7f648ed0
unknown <21b1num1604@stud.num.edu.mn>
2025-03-19 14:36:11 +0800

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className="text-red-500">
-     <h1>hello world</h1>
+    <div class="text-red-500">
+       <h1 class="text-3xl font-bold">
+    Hello world!
+  </h1>
     </div>
   )
 }
