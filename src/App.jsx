cbc43180eb8a5b36596626b3f3b9854546ec5ef3
unknown <21b1num1604@stud.num.edu.mn>
2025-03-19 15:57:29 +0800

    commited

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
-
+import React from 'react';
+import { BrowserRouter , Routes, Route } from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import Header from './components/Header';
 const App = () => {
   return (
-    <div class="text-red-500">
-       <h1 class="text-3xl font-bold">
-    Hello world!
-  </h1>
-    </div>
+    <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="profile" element={<Profile />} />
+      <Route path="signup" element={<SignUp />} />
+      <Route path="signin" element={<SignIn />} />
+    </Routes>
+    </BrowserRouter>
+
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import{Link} from 'react-router-dom';
+import { FaSearch } from 'react-icons/fa';
+const Header = () => {
+  return (
+    <header className='bg-slate-200 shadow-md'>
+        <div className="flex justify-between items-center max-w-6xl mx-auto p-4">
+            <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+                <span className="text-slate-400">Terra</span>
+                <span className="text-slate-700">Mind</span>
+            </h1>
+        <form className='bg-slate-100 p-3 rounded-2xl flex items-center'>
+            <input type="text" placeholder="Search" className="bg-transparent" />
+            <FaSearch className='text-slate-600'/>
+        </form>
+        <ul className='flex gap-4'>
+            <Link to="/"> 
+            <li className='inline-block mx-2'>
+                <a href="#" className='text-slate-600 hover:text-slate-700'>Home</a>
+            </li>
+            </Link>
+            <Link to="/about">
+            <li className='inline-block mx-2'>
+                <a href="#" className='text-slate-600 hover:text-slate-700'>About</a>
+            </li>
+            </Link>
+            <Link to="/profile">
+            <li className='inline-block mx-2'>
+                <a href="#" className='text-slate-600 hover:text-slate-700'>Profile</a>
+            </li>
+            </Link>
+            <Link to="/signup">
+            <li className='inline-block mx-2'>
+                <a href="#" className='text-slate-600 hover:text-slate-700'>Sign Up</a>
+            </li>
+            </Link>
+            <Link to="/signin">
+            <li className='inline-block mx-2'>
+                <a href="#" className='text-slate-600 hover:text-slate-700'>Sign In</a>
+            </li>
+            </Link>
+        </ul>
+        </div>
+    </header>
+  )
+}
+
+export default Header
